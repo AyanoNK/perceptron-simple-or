7c5cf7f698ebec834812eee8bfa9d9320f8ed57a
Ayano <s.correa.wilches@gmail.com>
2021-09-15 06:26:25 +0700

    Shifted from booleans to str

--- src/App.js
+++ src/App.js
@@ -1,76 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import {evaluate} from './utils/utils';
-import { makeStyles, createStyles, Box, FormControlLabel, Checkbox, Button, Typography } from '@material-ui/core';
+import logo from "./logo.svg";
+import "./App.css";
+import { evaluate } from "./utils/utils";
+import {
+  makeStyles,
+  createStyles,
+  Box,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-const useStyles = makeStyles(() => createStyles({
-  root: {
-    margin: 0,
-    fontFamily: ['Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',],
-    '-webkit-font-smoothing': 'antialiased',
-  '-moz-osx-font-smoothing': 'grayscale'
-  },
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      margin: 0,
+      fontFamily: [
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+      ],
+      "-webkit-font-smoothing": "antialiased",
+      "-moz-osx-font-smoothing": "grayscale",
+    },
+    color: {
+      color: "black",
+    },
+  })
+);
 
 function App() {
-
-  const styles = useStyles();
-  const [isZeroChecked, setIsZeroChecked] = useState(false);
-  const [isOneChecked, setIsOneChecked] = useState(false);
+  const classes = useStyles();
+  const [isZeroChecked, setIsZeroChecked] = useState("");
+  const [isOneChecked, setIsOneChecked] = useState("");
   const [arrayCheck, setArrayCheck] = useState([]);
 
   const [neuronResult, setNeuronResult] = useState(0);
+  const changeZero = (val) => {
+    setIsZeroChecked(val);
+  };
 
-  const changeZero = () => {
-    setIsZeroChecked(!isZeroChecked)
-  }
-
-  const changeOne = () => {
-    setIsOneChecked(!isOneChecked)
-  }
+  const changeOne = (val) => {
+    setIsOneChecked(val);
+  };
 
   const handleApply = () => {
-    setArrayCheck([isZeroChecked, isOneChecked])
-  }
+    setArrayCheck([isZeroChecked, isOneChecked]);
+  };
 
   useEffect(() => {
     const result = evaluate(arrayCheck);
     setNeuronResult(result);
   }, [arrayCheck]);
 
-
   return (
-    <Box className={styles.root}>
+    <Box className={classes.root}>
       <header className="App-header">
-        <FormControlLabel
-        control={
-          <Checkbox
-            checked={isZeroChecked}
-            onChange={changeZero}
-            name="checked_zero"
-            color="primary"
-          />
-        }
-        label="Zero"
-      />
-        <FormControlLabel
-        control={
-          <Checkbox
-            checked={isOneChecked}
-            onChange={changeOne}
-            name="checked_one"
-            color="secondary"
-          />
-        }
-        label="Uno"
-      />
-      <Button variant="contained" color="primary" onClick={handleApply}>
-        <Typography variant="h5">Enviar</Typography>
-      </Button>
-        <Typography variant="h5">
+        <TextField
+          id="filled-basic"
+          label="Primer valor"
+          type="number"
+          variant="filled"
+          className={classes.color}
+          onChange={(event) => changeZero(event.target.value)}
+        />
+        <TextField
+          id="filled-basic"
+          label="Segundo valor"
+          type="number"
+          variant="filled"
+          className={classes.color}
+          onChange={(event) => changeOne(event.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={handleApply}>
+          <Typography variant="h5">Enviar</Typography>
+        </Button>
+        <Typography variant="h5" className={classes.color}>
           {neuronResult}
         </Typography>
       </header>
